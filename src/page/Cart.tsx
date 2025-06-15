7c42155cf28160e89cb0a76ed67a672f477fc9e0
Joshua Wilson <joshdoesthis@icloud.com>
2025-06-15 13:32:01 +1200

    pages

--- src/page/Cart.tsx
+++ src/page/Cart.tsx
@@ -25,7 +25,7 @@ const Component = () => {
     <>
       <Header />
       <main>
-        <div className="inline-flex grow flex-col gap-y-8 bg-[#00806C]/50 p-8">
+        <div className="inline-flex grow flex-col bg-[#00806C]/50 p-8">
           <div className="mb-8 inline-flex flex-col gap-y-4 rounded-4xl bg-white p-8">
             {/* <div className="relative inline-flex items-center justify-end overflow-clip rounded-2xl bg-[#00806C]/10 p-8">
               <img className="absolute right-0 left-0 rounded-2xl" src={StoreMap} />
@@ -56,7 +56,7 @@ const Component = () => {
               </div>
             </div>
           </div>
-          <div className="inline-flex items-center gap-x-8 self-end rounded-2xl bg-white p-8">
+          <div className="inline-flex items-center gap-x-8 self-end rounded-4xl bg-white p-8">
             <span className="text-xl font-bold">Total: $3.75</span>
             <Link
               className="inline-flex items-center justify-center gap-x-2 rounded-2xl bg-[#F3C430] px-4 py-2 text-xl font-bold text-black"
