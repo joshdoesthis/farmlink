12130380888157057a8a5708f52718e005d1220c
Joshua Wilson <joshdoesthis@icloud.com>
2025-06-15 12:31:30 +1200

    pages

--- src/page/Browse.tsx
+++ src/page/Browse.tsx
@@ -1,20 +1,20 @@
 // import { Link } from "react-router-dom"
 
-// import Broccoli from "../../public/broccoli.png"
-// import Cabbage from "../../public/cabbage.png"
-// import Capsicum from "../../public/capsicum.png"
-// import Carrot from "../../public/carrot.png"
-// import Cauliflower from "../../public/cauliflower.png"
-// import Egg from "../../public/egg.png"
-// import Eggplant from "../../public/eggplant.png"
-// import Grape from "../../public/grape.png"
-// import Honey from "../../public/honey.png"
-// import Mushroom from "../../public/mushroom.png"
-// import Parsnip from "../../public/parsnip.png"
-// import Radish from "../../public/radish.png"
-// import Strawberry from "../../public/strawberry.png"
-// import Tomato from "../../public/tomato.png"
-// import Watermelon from "../../public/watermelon.png"
+import Broccoli from "../../public/broccoli.png"
+import Cabbage from "../../public/cabbage.png"
+import Capsicum from "../../public/capsicum.png"
+import Carrot from "../../public/carrot.png"
+import Cauliflower from "../../public/cauliflower.png"
+import Egg from "../../public/egg.png"
+import Eggplant from "../../public/eggplant.png"
+import Grape from "../../public/grape.png"
+import Honey from "../../public/honey.png"
+import Mushroom from "../../public/mushroom.png"
+import Parsnip from "../../public/parsnip.png"
+import Radish from "../../public/radish.png"
+import Strawberry from "../../public/strawberry.png"
+import Tomato from "../../public/tomato.png"
+import Watermelon from "../../public/watermelon.png"
 import Header from "../component/Header"
 
 const Component = () => {
@@ -22,7 +22,130 @@ const Component = () => {
     <>
       <Header />
       <main>
-        <div className="grid grow grid-flow-col items-start gap-8 bg-[#00806C]/50 p-8"></div>
+        <div className="grow bg-[#00806C]/50 p-8">
+          <div className="grid grid-cols-3 gap-8">
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Broccoli} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Broccoli</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Cabbage} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Cabbage</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Capsicum} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Capsicum</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Carrot} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Carrot</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Cauliflower} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Cauliflower</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Egg} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Egg</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Eggplant} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Eggplant</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Grape} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Grape</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Honey} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Honey</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Mushroom} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Mushroom</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Parsnip} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Parsnip</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Radish} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Radish</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Strawberry} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Strawberry</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Tomato} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Tomato</span>
+              </div>
+            </div>
+            <div className="inline-flex flex-col items-center justify-center gap-4 overflow-clip rounded-4xl bg-white p-8">
+              <div>
+                <img className="h-12" src={Watermelon} />
+              </div>
+              <div className="inline-flex flex-col items-start justify-between gap-x-4">
+                <span className="text-2xl font-bold">Watermelon</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   )
